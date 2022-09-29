b398c5d83d0d0621d9bcdf5fb1972b1e5b4674ef
alnig441 <alnig441@gmail.com>
2022-09-29 15:17:21 -0400

    endpoint changed to herokuapp

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -11,7 +11,7 @@ if(browser) {
 export const  chatLog = writable([]),
               deviceWidth = readable(innerWidth),
               animated = writable(false),
-              socket = readable(io("http://localhost:3000")),
+              socket = readable(io("http://allan-nielsen-chat-app.herokuapp.com")),
               disabled = writable(false),
               collapsed = writable(false),
               alert = writable(false),
